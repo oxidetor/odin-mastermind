76421c54e564b2953fb03572d537cc593da94ed0
oxidetor <prateek.girdhar@outlook.com>
2022-07-01 19:23:12 -0700

    Draw turn indicators and populate each board line

--- board.rb
+++ board.rb
@@ -1,6 +1,9 @@
 class Board
+  attr_accessor :played_lines
+
   def initialize(game)
     @game = game
+    @played_lines = []
   end
 
   def check_guess
@@ -13,21 +16,43 @@ class Board
   end
 
   def draw_board
-    12.times do
-      draw_line
+    draw_empty_lines
+    draw_current_line
+    draw_played_lines
+    played_lines.push [@game.guess, @pegs, 12 - @game.turns + 1]
+  end
+
+  def draw_empty_lines
+    (@game.turns - 1).times do |index|
+      if index == @game.turns - 2
+        puts draw_holes(%w[_ _ _ _], '==>') + draw_pegs([]).join(' ')
+
+      else
+        puts draw_holes(%w[_ _ _ _], 12 - index) + draw_pegs([]).join(' ')
+      end
     end
   end
 
-  def draw_line
-    puts draw_holes + draw_pegs.to_s
+  def draw_current_line
+    puts draw_holes(@game.guess, 12 - @game.turns + 1) + draw_pegs(@pegs).join(' ')
   end
 
-  def draw_holes
-    "#{@game.guess}\t| _ | _ | _ | _ |\t"
+  def draw_played_lines
+    played_lines.reverse.each do |line|
+      draw_played_line(line)
+    end
   end
 
-  def draw_pegs
-    @pegs
+  def draw_played_line(line)
+    puts draw_holes(line[0], line[2]) + draw_pegs(line[1]).join(' ')
+  end
+
+  def draw_holes(guess, turn_number)
+    " #{turn_number}\t| #{guess[0]} | #{guess[1]} | #{guess[2]} | #{guess[3]} |\t"
+  end
+
+  def draw_pegs(pegs)
+    pegs
   end
 
   def generate_pegs(right_positions, right_colors)
